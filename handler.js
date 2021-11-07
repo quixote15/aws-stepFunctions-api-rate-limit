81c0ff43f8a0cce7e4ac708433e70cecc4486a30
quixote15 <santos2tiago@gmail.com>
2021-11-07 10:57:28 -0300

    feature: config cloud watch event every minute

--- handler.js
+++ handler.js
@@ -2,56 +2,107 @@
 const faker = require('faker');
 console.log("Loading function");
 var AWS = require("aws-sdk");
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05', region: 'us-east-1' })
+const {
+  MaxPriorityQueue
+} = require('@datastructures-js/priority-queue');
 
 module.exports.banklyRequest = async function(event, context) {
     var eventText = JSON.stringify(event, null, 2);
     console.log("Received event:", eventText);
     const partnerKeys = ['dev/misc-bankly/keys/gilmarBank', 'dev/misc-bankly/keys/PeuBank']
-    const keyIndex = Math.round(Math.random() * 1)
+   
     var sns = new AWS.SNS();
-    const messagePayload = {
-      type: 'bankSlip',
-      service: 'cashIn',
-      data: {
-        amount: 100,
-        account: faker.finance.account,
+   
+    const n = Array.from(Array(10000).keys())
+    const promises = [];
+    console.log('items: ', n)
+    for (let item of n) {
+      try {
+        console.log("index: ", item)
+        const keyIndex = Math.round(Math.random() * 1)
+        const messagePayload = {
+          type: 'bankSlip',
+          service: 'cashIn',
+          credentialName: partnerKeys[keyIndex],
+          priority: keyIndex,
+          data: {
+            amount: Math.random() * 1000,
+            account: faker.finance.account,
+          }
+        }
+        var params = {
+            Message: JSON.stringify(messagePayload), 
+            MessageGroupId: partnerKeys[keyIndex],
+            MessageDeduplicationId: faker.random.alphaNumeric(20) + Date.now(),
+            Subject: "Bankly Request",
+            TopicArn: "arn:aws:sns:us-east-1:335006469695:NewApiRequest.fifo"
+        };
+    
+        promises.push(sns.publish(params).promise());
+        console.log('enviou, ', item)
+      } catch (error) {
+        console.error(error);
       }
     }
-    var params = {
-        Message: JSON.stringify(messagePayload), 
-        MessageGroupId: partnerKeys[keyIndex],
-        MessageDeduplicationId: faker.random.alphaNumeric(20),
-        Subject: "Bankly Request",
-        TopicArn: "arn:aws:sns:us-east-1:335006469695:NewApiRequest.fifo"
-    };
-    try {
-      const data = await sns.publish(params, context.done).promise();
-      console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-      console.log("MessageID is " + data.MessageId);
-    } catch (error) {
-      console.error(err, err.stack);
-    }
+
+    await Promise.all(promises).catch(console.log)
+
     return 'Requisição enviada.'
 }
 module.exports.pollRequestsAndCheckQuotas = async (event) => {
- 
- const requests = [{
-   type: 'bankSlip',
-   service: 'cashIn',
-   data: {
-     amount: 10,
-     account: 1
-   }
- }, {
-  type: 'bankSlip',
-  service: 'cashIn',
-  data: {
-    amount: 100,
-    account: 2
+  const params = {
+    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/335006469695/ApiRequestsQueue.fifo'
   }
-}]
 
-return requests
+  const messagesConsumed = []
+  const requestMap = new Map()
+  try {
+    const receiveResp = await sqs.receiveMessage({...params, WaitTimeSeconds: 5}).promise()
+    console.log('Fila retornou:')
+    console.log(receiveResp)
+    console.log(receiveResp.Messages)
+    receiveResp.Messages.forEach(m => {
+      const notificationMessage = JSON.parse(m.Body)
+      console.log('notificationMessage: ' + notificationMessage)
+      const request = JSON.parse(notificationMessage.Message)
+      console.log('request',request)
+      const deleteMessageParams = {
+        ...params,
+        ReceiptHandle: m.ReceiptHandle
+      }
+
+      messagesConsumed.push(deleteMessageParams)
+      const partnerKey = request.credentialName;
+
+      if(requestMap.has(partnerKey)) {
+        const partnerRequestQueue = requestMap.get(partnerKey)
+        partnerRequestQueue.enqueue({request, message: m}, request.priority )
+        requestMap.set(partnerKey, partnerRequestQueue)
+      }else {
+        const partnerQueue = new MaxPriorityQueue()
+        partnerQueue.enqueue({request, message: m}, request.priority)
+        requestMap.set(partnerKey, partnerQueue)
+      }
+    })
+  } catch (error) {
+    console.error('error queue: ', error)
+  }
+
+
+ const messageDeletePromises = messagesConsumed.map(m => sqs.deleteMessage(m).promise())
+
+ await Promise.all(messageDeletePromises)
+
+ const requests = []
+
+ for(let [key, queue] of requestMap.entries()) {
+   console.log(key)
+   requests.push(queue.toArray())
+ }
+
+  
+  return requests
 };
 
 
